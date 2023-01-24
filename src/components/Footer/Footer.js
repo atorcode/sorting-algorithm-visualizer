8d299607fa452aa8aadb2d93cb3899f1c970476d
atorcode <atorcode@gmail.com>
2023-01-24 01:04:36 -0800

    Add Contact Me to footer and improve layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,26 +5,31 @@ import WikipediaIcon from "../../media/wikipedia.png";
 const Footer = () => {
   return (
     <footer className={styles["footer"]}>
-      <section>
-        <h2>Contact Me</h2>
-      </section>
-      <section>
-        <h2>References</h2>
-        <div>
-          <FaGithub className={styles["github-icon"]} />
-          <p>GitHub</p>
-        </div>
-        <div>
-          <img
-            src={WikipediaIcon}
-            alt="Wikipedia logo"
-            height="40px"
-            width="40px"
-          />
-          <p>Wikipedia</p>
-        </div>
-      </section>
-      <p>&copy; 2023 Alberto Torrigiotti</p>
+      <article>
+        <section>
+          <h2>Contact Me</h2>
+          <p className={styles["margin-top"]}>atorcode@gmail.com</p>
+        </section>
+        <section>
+          <h2>References</h2>
+          <div className={styles["margin-top"]}>
+            <FaGithub className={styles["github-icon"]} />
+            <p>GitHub</p>
+          </div>
+          <div className={styles["margin-top"]}>
+            <img
+              src={WikipediaIcon}
+              alt="Wikipedia logo"
+              height="40px"
+              width="40px"
+            />
+            <p>Wikipedia</p>
+          </div>
+        </section>
+      </article>
+      <p className={styles["copyright-text"]}>
+        &copy; 2023 Alberto Torrigiotti
+      </p>
     </footer>
   );
 };
