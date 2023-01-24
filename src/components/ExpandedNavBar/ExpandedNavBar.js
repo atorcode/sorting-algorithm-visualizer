26594f93a9ab0727025318655bccb88cb45ad430
atorcode <atorcode@gmail.com>
2023-01-24 10:00:39 -0800

    Fix hover highlighting for expanded nav bar

--- src/components/ExpandedNavBar/ExpandedNavBar.js
+++ src/components/ExpandedNavBar/ExpandedNavBar.js
@@ -5,21 +5,32 @@ import { TbAntennaBars5 } from "react-icons/tb";
 const ExpandedNavBar = () => {
   return (
     <nav className={styles["expanded-nav-bar"]}>
-      <section className={styles["home-section"]}>
-        <MdHome className={styles["icons"]} />
-        <h2>Home</h2>
-      </section>
-      <section>
+      <a href="/">
+        <div className={styles["home"]}>
+          <MdHome className={styles["icons"]} />
+          <h2>Home</h2>
+        </div>
+      </a>
+      <div className={styles["divider-line"]}></div>
+      <div className={styles["algorithms"]}>
         <TbAntennaBars5 className={styles["icons"]} />
         <h2>Algorithms</h2>
         {/* following list is formatted with flex-wrap to have desired styling while being a child of this section element, which is more semantic. Could alternatively use grid instead of flex-wrap, but the width of the nav bar is constant, so this solution works fine. */}
-        <ul>
-          <li>Quick Sort</li>
-          <li>Merge Sort</li>
-          <li>Bubble Sort</li>
-          <li>Selection Sort</li>
-        </ul>
-      </section>
+      </div>
+      <ul>
+        <li>
+          <a href="#">Quick Sort</a>
+        </li>
+        <li>
+          <a href="#">Merge Sort</a>
+        </li>
+        <li>
+          <a href="#">Bubble Sort</a>
+        </li>
+        <li>
+          <a href="#">Selection Sort</a>
+        </li>
+      </ul>
     </nav>
   );
 };
