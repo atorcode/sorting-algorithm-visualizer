a4c6666a73d5275708f38eec0404cd79aa876755
atorcode <atorcode@gmail.com>
2023-01-24 03:51:15 -0800

    Begin implementation of letter scrambling function

--- src/components/HeroHeading/HeroHeading.js
+++ src/components/HeroHeading/HeroHeading.js
@@ -1,6 +1,32 @@
 import styles from "./HeroHeading.module.scss";
+import { useEffect } from "react";
+import { HEROHEADING, LETTERS } from "../../constants";
 
 const HeroHeading = () => {
+  const getRandomLetter = () => {
+    console.log(LETTERS[Math.floor(Math.random() * 26)]);
+    return LETTERS[Math.floor(Math.random() * 26)];
+  };
+
+  const scrambleLetters = (str) => {
+    const arr = str.split();
+
+    arr.forEach((char) => {
+      if (char !== "") {
+        char = getRandomLetter();
+      }
+    });
+  };
+
+  useEffect(() => {
+    // const intervalId = setInterval(() => {
+    //   scrambleLetters(HEROHEADING);
+    // }, 500);
+    // return () => {
+    //   clearInterval(intervalId);
+    // };
+  }, []);
+
   return <h1 className={styles["heading"]}>SORTING ALGORITHM VISUALIZER</h1>;
 };
 
