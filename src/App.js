3c3ac4bdf84f69d9fe65ed4edca82e4168c23aeb
atorcode <atorcode@gmail.com>
2023-01-24 06:59:15 -0800

    Add toggle functionality to expanded nav bar button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "./App.scss";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
-import ExpandedNavBar from "./components/ExpandedNavBar";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* conditionally render later */}
-      {/* <ExpandedNavBar /> */}
       <Hero />
       <Footer />
     </div>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,29 @@
 import styles from "./NavBar.module.scss";
 import HamburgerButton from "../HamburgerButton";
+import ExpandedNavBar from "../ExpandedNavBar/ExpandedNavBar";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
   return (
-    <nav className={styles["nav-bar"]}>
-      <a href="/">
-        <p>SORTING ALGORITHM VISUALIZER</p>
-      </a>
-      <HamburgerButton />
-    </nav>
+    <>
+      <nav className={styles["nav-bar"]}>
+        <a href="/">
+          <p className={styles["extra-padding"]}>
+            SORTING ALGORITHM VISUALIZER
+          </p>
+        </a>
+        <button
+          className={styles["extra-padding"]}
+          onClick={() => {
+            setIsExpanded(!isExpanded);
+          }}
+        >
+          <HamburgerButton />
+        </button>
+      </nav>
+      {isExpanded && <ExpandedNavBar />}
+    </>
   );
 };
 
