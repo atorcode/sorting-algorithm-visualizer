9ce7283dbdccca3c6aa4068da51488c8c053daf8
atorcode <atorcode@gmail.com>
2023-01-30 23:24:39 -0800

    Create code block for AlgorithmImplementation component

--- src/components/AlgorithmImplementation/AlgorithmImplementation.js
+++ src/components/AlgorithmImplementation/AlgorithmImplementation.js
@@ -2,16 +2,19 @@ import styles from "./AlgorithmImplementation.module.scss";
 
 const AlgorithmImplementation = () => {
   return (
-    <section>
+    <section className={styles["implementation-section"]}>
       <h2>IMPLEMENTATION</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-        cumque odio magni perferendis ab fugit assumenda rem maiores quibusdam
-        quaerat, delectus nulla temporibus facere nihil, cum neque laborum eum
-        voluptas distinctio. Quibusdam soluta alias consequatur laborum, fugiat
-        sapiente quod mollitia exercitationem suscipit corrupti aliquam nesciunt
-        minima voluptas laboriosam beatae animi.
-      </p>
+      <div>
+        <pre>
+          <code>
+            {`
+    const someAlg = () => {
+              
+    };
+            `}
+          </code>
+        </pre>
+      </div>
     </section>
   );
 };
