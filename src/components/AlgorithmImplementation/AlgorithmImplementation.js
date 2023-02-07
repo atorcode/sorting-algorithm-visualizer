16b077db2d79c8b94d8d679c6cefc0c1906d2708
atorcode <atorcode@gmail.com>
2023-02-07 01:48:16 -0800

    Format algorithm implementation and complete insertion sort details

--- src/components/AlgorithmImplementation/AlgorithmImplementation.js
+++ src/components/AlgorithmImplementation/AlgorithmImplementation.js
@@ -1,15 +1,14 @@
 import styles from "./AlgorithmImplementation.module.scss";
 
 const AlgorithmImplementation = ({ implementation }) => {
+  const createMarkup = () => {
+    return { __html: implementation };
+  };
   return (
     <section className={styles["implementation-section"]}>
       <h2>IMPLEMENTATION</h2>
       <pre>
-        <code>
-          {`
-    ${implementation}
-            `}
-        </code>
+        <code dangerouslySetInnerHTML={createMarkup()}></code>
       </pre>
     </section>
   );
